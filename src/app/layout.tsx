667a3c9e39206f590a7cba898e05e924e4d24e09
GROSJOINDEBEUH <mosiah54@yahoo.fr>
2026-05-26 00:21:08 +0200

    feat: ajout de vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { siteConfig } from "@/config/site";
 import { Header } from "@/components/layout/header";
 import "./globals.css";
@@ -79,6 +80,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
